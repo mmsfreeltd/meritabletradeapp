35121b7b70e79a44b4711f53047928e477c9cfb3
dogunfx <dogunfx@gmail.com>
2025-11-03 14:46:16 +0100

    completed meritable set

--- global/constants.ts
+++ global/constants.ts
@@ -1,14 +1,22 @@
-export const SITE_NAME: string = "Prolific Quant";
-export const SITE_URL: string = "app.prolificquant.com";
-export const PROFILE_IMAGE_PLACE_HOLDER: string = "/images/profile.jpg";
+export const SITE_NAME: string = 'Meritable Stock Trades ';
+export const SITE_URL: string = 'meritablestocktradesapp.vercel.app';
+export const PROFILE_IMAGE_PLACE_HOLDER: string = '/images/profile.jpg';
 export const SITE_URL_WITH_HTTPS: string = `https://${SITE_URL}`;
 export const LIVE_CHAT = `
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '0345d336f42ed1d836a7d0c19f47b41d468271e7';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = 'cd5df0fc8a0ee288558ecc0636cea078af5e0b68';
+    window.smartsupp || (function(d) {
+        var s, c, o = smartsupp = function() {
+            o._.push(arguments)
+        };
+        o._ = [];
+        s = d.getElementsByTagName('script')[0];
+        c = d.createElement('script');
+        c.type = 'text/javascript';
+        c.charset = 'utf-8';
+        c.async = true;
+        c.src = 'https://www.smartsuppchat.com/loader.js?';
+        s.parentNode.insertBefore(c, s);
+    })(document);
+
   `;
