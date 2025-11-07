f7b8fbfbf43ef2003ba6aa151f0f4f1ead6c2ad2
dogunfx <dogunfx@gmail.com>
2025-11-07 14:54:08 +0100

    added livechat

--- global/constants.ts
+++ global/constants.ts
@@ -3,4 +3,12 @@ export const SITE_URL: string = 'app.greenlinkmarkets.com';
 export const PROFILE_IMAGE_PLACE_HOLDER: string = '/images/profile.jpg';
 export const SITE_URL_WITH_HTTPS: string = `https://${SITE_URL}`;
 export const LIVE_CHAT = `
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '6afad7ba58fa92613bf550138f96247b0f8b7404';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
   `;
